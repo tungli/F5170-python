09075170f3315d5c8bb7e3471ecc6e312555000c
tungli <424020@mail.muni.cz>
2018-09-17 20:24:56 +0200

    van_allen script plots and parameters

--- 3_Motion/van_allen.py
+++ 3_Motion/van_allen.py
@@ -1,6 +1,8 @@
 from scipy import integrate as itg
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 def derivs(y,t,E,q,m,magM):
     d = np.zeros(np.shape(y))
@@ -9,27 +11,60 @@ def derivs(y,t,E,q,m,magM):
     return d
 
 def bfield(r,M):
-    #This function calculates the magnetic field from the magnetic moment
+    #This function calculates the magnetic field from 
+    #position vector and magnetic dipole moment
+    #Good luck!
 
 
 
+E = np.array([0.0,0.0,0.0])
+magM = np.array([0.0,0.0,8.10e22])
+q = 1.60217662e-19
+m = 1.6726219e-27 
+re = 6.38e6
 
-E = np.array([1.0,0.0,0.0])
-magM = np.array([0.0,0.0,1.0])
-q = 1.0
-m = 1.0
+#Velocity
+vr = 1.0e7
+vp = 0.0
+vt = np.pi/4
+v = np.array([vr*np.sin(vt)*np.cos(vp),vr*np.sin(vt)*np.sin(vp),vr*np.cos(vt)])
+#Position
+rr = 2.5*re
+rp = 0.0
+rt = np.pi/2
+r = np.array([rr*np.sin(rt)*np.cos(rp),rr*np.sin(rt)*np.sin(rp),rr*np.cos(rt)])
+#State vector
+y0 = np.array([r[0],r[1],r[2],v[0],v[1],v[2]])
 
 ti = 0.0
-tf = 10.0
+tf = 20.0
 num_points = 1000
 t = np.linspace(ti,tf,num_points)
 
-y0 = np.array([1.0,1.0,0.0,0.0,0.0,0.0])
 res = itg.odeint(derivs,y0,t,args=(E,q,m,magM))
 
-x = [ i[0] for i in res ]
-y = [ i[1] for i in res ]
-z = [ i[2] for i in res ]
+x = [ i[0]/re for i in res ]
+y = [ i[1]/re for i in res ]
+z = [ i[2]/re for i in res ]
 
-plt.plot(y,x)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+#Plot the ball
+u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+xs = np.cos(u)*np.sin(v)
+ys = np.sin(u)*np.sin(v)
+zs = np.cos(v)
+ax.plot_surface(xs, ys, zs, cmap=cm.plasma)
+#Note: the colormap "plasma" is the only acceptable colormap when doing plasma physics!
+
+#Plot data
+ax.plot(x, y, z, lw=1.0, c="b")
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+#ax.set_xlim((-3.0,3.0))
+#ax.set_ylim((-3.0,3.0))
+#ax.set_zlim((-3.0,3.0))
+
 plt.show(block=True)
